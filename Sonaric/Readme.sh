9ea540288781f599941f20b3cda76b3572f93df0
TauroDeLline <150973178+TauroDeLline@users.noreply.github.com>
2024-12-22 14:48:01 +0100

    Create Readme.sh

---
+++ Sonaric/Readme.sh
@@ -0,0 +1,4 @@
+## Запуск
+
+``` bash
+wget -O Sonaric_Install https://raw.githubusercontent.com/TauroDeLline/Nodes/main/Sonaric/Sonaric_Install.sh && chmod +x Sonaric_Install.sh && ./Sonaric_Install.xh
